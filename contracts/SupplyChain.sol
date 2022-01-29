83bc32fcc8bc353d24e9e35bac7395abd0120280
Privateer Technologies <privatepool@protonmail.com>
2022-01-29 15:51:11 -0300

    log7

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -123,9 +123,9 @@ contract SupplyChain {
   //    - check the value after the function is called to make 
   //      sure the buyer is refunded any excess ether sent. 
   // 6. call the event associated with this function!
-  function buyItem(uint _sku) public payable paidEnough(items[_sku].price) forSale(items[_sku].state, items[_sku].price) checkValue(_sku)
+  function buyItem(uint _sku) public payable forSale(items[_sku].state, items[_sku].price) checkValue(_sku) paidEnough(items[_sku].price)
   {
-    require(msg.value >=items[_sku].price);
+    require(msg.value >= items[_sku].price);
     items[_sku].seller.transfer(items[_sku].price);
     items[_sku].buyer = msg.sender;
     items[_sku].state = State.Sold;
@@ -150,6 +150,7 @@ contract SupplyChain {
   // 3. Call the event associated with this function!
   function receiveItem(uint _sku) public shipped(items[_sku].state) verifyCaller(items[_sku].buyer)
   {
+    require(msg.sender == items[_sku].buyer);
     items[_sku].state = State.Received;
     emit LogReceived(_sku);
   }
